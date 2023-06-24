b219143836905aed0c65427fa6c558e294287648
Nataly Araujo <ntlyaraujo@gmail.com>
2023-06-24 19:42:27 +0200

    Implement section with header and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
 import '@styles/global.css'
+
 export const metadata = {
-    title: "Imagina Bot",
+    title: "Imagine",
     description: "Descubra e compartilhe prompts para IA",
 }
 
-interface RootLayoutProps {
+type RootLayoutProps = {
     children: string
 }
 
 const RootLayout = ({children}: RootLayoutProps) => {
     return (
-       <html lang="pt_BR">
-              <head>
-                <title>{metadata.title}</title>
-                <meta name="description" content={metadata.description} />
-              </head>
-       <body>
-       <main className="app">
-           {children}
-       </main>
-       </body>
-       </html>
+        <html lang="pt_BR">
+        <head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description}/>
+        </head>
+        <body>
+        <div className='main'>
+            <div className='gradient'/>
+        </div>
+
+        <main className="app">
+            {children}
+        </main>
+        </body>
+        </html>
 
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
     return (
-        <section className="w-full font-sans container mx-auto p-10">
-            <h1 className="text-center ">Descubra & Compartilhe</h1>
+        <section
+            className="flex flex-col items-center justify-center font-sans container mx-auto p-10">
+            <h1 className="text-center head_text">
+                Descubra & Compartilhe
+                <br/>
+                <span className="purple_gradient text-center">Prompts para IA</span>
+            </h1>
+            <p className="desc text-center w-full">
+                Imagine é uma plataforma para criar, compartilhar e descobrir prompts para chatGPT, Midjourney, etc.
+            </p>
+            <Feed/>
         </section>
     )
 }
